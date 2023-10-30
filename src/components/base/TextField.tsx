59167c95cdee250ff24cd7fb88d28eceabddf53c
irmanfreestyle <irmanfreestyle7@gmail.com>
2023-10-30 20:41:22 +0700

    Grouping transactions by month and render it

--- src/components/base/TextField.tsx
+++ src/components/base/TextField.tsx
@@ -1,9 +1,11 @@
-import { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, Ref } from "react";
 
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export default function TextField(props: TextFieldProps) {
+const TextField = React.forwardRef((props: TextFieldProps, ref: Ref<HTMLInputElement>) => {
   return (
-    <input {...props} className="bg-white rounded-lg border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full px-3 py-2" />
+    <input ref={ref} {...props} className="bg-white rounded-lg border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full px-3 py-2" />
   )
-}
+})
+
+export default TextField;
---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+export const randomId = function(length = 6): string {
+  return Math.random().toString(36).substring(2, length+2);
+};
+
+export const toDate = (date: string) => {
+  return moment(date).format('DD MMMM YYYY');
+}
+
+export const toRupiah = (value: number) => {
+  const formatter = new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  });
+
+  return formatter.format(value);
+}
